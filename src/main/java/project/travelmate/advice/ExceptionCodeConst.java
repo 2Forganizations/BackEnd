aeb58dd65243aaf2ded74ca2a334602b44757962
taegyun1995 <needle12s@gmail.com>
2023-04-14 13:38:37 +0900

    fix: Exception Code Const -> enum

--- src/main/java/project/travelmate/advice/ExceptionCodeConst.java
+++ src/main/java/project/travelmate/advice/ExceptionCodeConst.java
@@ -1,7 +1,19 @@
 package project.travelmate.advice;
 
-public class ExceptionCodeConst {
+import lombok.Getter;
 
-    public final String EXAMPLE_CODE = "NOT_EXIST_USER";
+@Getter
+public enum ExceptionCodeConst {
+
+    EXAMPLE_INVALID_CODE("EXAMPLE", "EXAMPLE"),
+    USER_NOT_FOUND("USER_NOT_FOUND", "유저를 찾을 수 없습니다.");
+
+    private final String code;
+    private final String message;
+
+    ExceptionCodeConst(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 
 }
