089a7373cc91a6bdc77fe1e3be833c47baae519a
taegyun1995 <needle12s@gmail.com>
2023-04-20 14:52:38 +0900

    feat: CodeConst By Exception

--- src/main/java/project/travelmate/advice/ExceptionCodeConst.java
+++ src/main/java/project/travelmate/advice/ExceptionCodeConst.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum ExceptionCodeConst {
 
     EXAMPLE_INVALID_CODE("EXAMPLE", "EXAMPLE"),
-    USER_NOT_FOUND("USER_NOT_FOUND", "유저를 찾을 수 없습니다.");
+    USER_NOT_FOUND_CODE("USER_NOT_FOUND", "유저를 찾을 수 없습니다."),
+    OAUTH_NOT_SUPPORT_CODE("OAUTH_NOT_SUPPORT", "지원되지 않는 인증입니다."),
+    EXPIRED_ACCESS_TOKEN_CODE("EXPIRED_ACCESS_TOKEN", "기한이 만료된 토큰입니다.");
 
     private final String code;
     private final String message;
