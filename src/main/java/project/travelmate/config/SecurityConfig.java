205988ed342ce605b718bbbf295006798b9890f9
taegyun1995 <needle12s@gmail.com>
2023-04-20 14:53:36 +0900

    feat: Spring Security & WebClient Config

--- src/main/java/project/travelmate/config/SecurityConfig.java
+++ src/main/java/project/travelmate/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package project.travelmate.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -13,5 +18,27 @@ public class SecurityConfig {
                 "/**"
         );
     }
-    
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .cors()
+                .and()
+                .sessionManagement()//세션 정책 설정
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .redirectionEndpoint()
+                .baseUri("/oauth2/code/*");
+
+        return http.build();
+    }
+
 }
