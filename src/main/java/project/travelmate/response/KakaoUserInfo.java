ddf6c5a0211a66bb2659d8708336a654ddb0dfa1
taegyun1995 <needle12s@gmail.com>
2023-04-22 16:44:39 +0900

    fix: kakao UserInfo Response

--- src/main/java/project/travelmate/response/KakaoUserInfo.java
+++ src/main/java/project/travelmate/response/KakaoUserInfo.java
@@ -1,21 +1,25 @@
 package project.travelmate.response;
 
-import lombok.AccessLevel;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoUserInfo {
 
     private String id;
-    private String email;
-    private String name;
+    private KakaoAccount kakaoAccount;
 
-    public KakaoUserInfo(String id, String email, String name) {
-        this.id = id;
-        this.email = email;
-        this.name = name;
+    @Getter
+    private static class KakaoAccount {
+        private String email;
+        private Profile profile;
+
+        @Getter
+        private static class Profile {
+            private String nickname;
+        }
     }
 
 }
