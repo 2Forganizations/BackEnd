729956b7d811cb0a84cb63e4ed0af7a0f25a45fe
taegyun1995 <needle12s@gmail.com>
2023-04-28 12:35:37 +0900

    fix: securiry filter update

--- src/main/java/project/travelmate/filter/SecurityFilter.java
+++ src/main/java/project/travelmate/filter/SecurityFilter.java
@@ -1,24 +1,28 @@
 package project.travelmate.filter;
 
 import com.nimbusds.jose.shaded.json.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import project.travelmate.advice.exception.ExpiredTokenException;
-import project.travelmate.advice.exception.UserNotFoundException;
 import project.travelmate.repository.UserRepository;
+import project.travelmate.request.AuthInfo;
 import project.travelmate.util.SecurityUtil;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
 
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static project.travelmate.advice.ExceptionCodeConst.EXPIRED_ACCESS_TOKEN_CODE;
-import static project.travelmate.advice.ExceptionCodeConst.USER_NOT_FOUND_CODE;
-import static project.travelmate.domain.enums.AuthProvider.findByCode;
 
+@Slf4j
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
@@ -42,37 +46,32 @@ public class SecurityFilter extends OncePerRequestFilter {
                 if (securityUtil.isExpiration(token)) {
                     throw new ExpiredTokenException(EXPIRED_ACCESS_TOKEN_CODE);
                 }
-
-                String userId = (String) securityUtil.get(token).get("userId");
+                String memberId = (String) securityUtil.get(token).get("id");
                 String provider = (String) securityUtil.get(token).get("provider");
 
-                if (userId == null ||
-                        provider == null ||
-                        !userRepository.existsByIdAndAuthProvider(userId, findByCode(provider))) {
-                    throw new UserNotFoundException(USER_NOT_FOUND_CODE);
-                }
+                AuthInfo authInfo = new AuthInfo(memberId, provider, token);
+                SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(authInfo, null, null));
             }
+
             filterChain.doFilter(request, response);
         } catch (ExpiredTokenException e) {
             if (e.getMessage().equalsIgnoreCase(EXPIRED_ACCESS_TOKEN_CODE.getMessage())) {
                 JSONObject jsonObject = createJsonError(String.valueOf(UNAUTHORIZED.value()), e.getMessage());
                 setJsonResponse(response, UNAUTHORIZED, jsonObject.toString());
             }
-        } catch (UserNotFoundException e) {
-            if (e.getMessage().equalsIgnoreCase(USER_NOT_FOUND_CODE.getMessage())) {
-                JSONObject jsonObject = createJsonError(String.valueOf(UNAUTHORIZED.value()), e.getMessage());
-                setJsonResponse(response, UNAUTHORIZED, jsonObject.toString());
-            }
         } catch (Exception e) {
+            writeErrorLogs("Exception", e.getMessage(), e.getStackTrace());
+
             if (response.getStatus() == HttpStatus.OK.value()) {
                 response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
             }
+        } finally {
+            log.debug("**** SECURITY FILTER FINISH");
         }
     }
 
     private JSONObject createJsonError(String errorCode, String errorMessage) {
         JSONObject jsonObject = new JSONObject();
-
         jsonObject.put("error_code", errorCode);
         jsonObject.put("error_message", errorMessage);
 
@@ -82,6 +81,20 @@ public class SecurityFilter extends OncePerRequestFilter {
     private void setJsonResponse(HttpServletResponse response, HttpStatus httpStatus, String jsonValue) {
         response.setStatus(httpStatus.value());
         response.setContentType(APPLICATION_JSON_VALUE);
+
+        try {
+            response.getWriter().write(jsonValue);
+            response.getWriter().flush();
+            response.getWriter().close();
+        } catch (IOException ex) {
+            writeErrorLogs("IOException", ex.getMessage(), ex.getStackTrace());
+        }
+    }
+
+    private void writeErrorLogs(String exception, String message, StackTraceElement[] stackTraceElements) {
+        log.error("**** " + exception + " ****");
+        log.error("**** error message : " + message);
+        log.error("**** stack trace : " + Arrays.toString(stackTraceElements));
     }
 
 }
