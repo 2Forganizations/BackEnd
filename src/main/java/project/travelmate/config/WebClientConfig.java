cb688663c274eac288a9b684c6010ba5f32c84e8
taegyun1995 <needle12s@gmail.com>
2023-04-28 20:02:21 +0900

    feat: login interceptor bean create

--- src/main/java/project/travelmate/config/WebClientConfig.java
+++ src/main/java/project/travelmate/config/WebClientConfig.java
@@ -3,6 +3,8 @@ package project.travelmate.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import project.travelmate.interceptor.LoginInterceptor;
+import project.travelmate.util.JwtUtil;
 
 @Configuration
 public class WebClientConfig {
@@ -10,6 +12,11 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder().build();
+    }
+
+    @Bean
+    public LoginInterceptor loginInterceptor(JwtUtil jwtUtil) {
+        return new LoginInterceptor(jwtUtil);
     }
 
 }
