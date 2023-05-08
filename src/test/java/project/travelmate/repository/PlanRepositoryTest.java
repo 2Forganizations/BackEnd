f90c2172c9911d4aa72e5e6f4c52a478f371dd4f
taegyun1995 <needle12s@gmail.com>
2023-05-08 12:02:23 +0900

    test: plan save repository

---
+++ src/test/java/project/travelmate/repository/PlanRepositoryTest.java
@@ -0,0 +1,23 @@
+package project.travelmate.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import project.travelmate.domain.Plan;
+import project.travelmate.domain.enums.Category;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PlanRepositoryTest extends RepositoryTest {
+
+    @DisplayName("여행 계획 생성 API - Repository")
+    @Test
+    void plan_save_repository() {
+        Plan savedPlan = planRepository.save(plan);
+
+        assertNotNull(savedPlan.getId());
+        assertThat(savedPlan).isNotNull();
+        assertThat(savedPlan.getCategory()).isEqualTo(Category.EAT);
+    }
+
+}
---
+++ src/test/java/project/travelmate/repository/RepositoryTest.java
@@ -0,0 +1,70 @@
+package project.travelmate.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import project.travelmate.domain.*;
+
+import static project.travelmate.utils.constant.Constants.*;
+
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class RepositoryTest {
+
+    public ProfileImage profileImage;
+    public User user;
+    public PlanImage planImage;
+    public Plan plan;
+
+    @Autowired
+    protected PlanRepository planRepository;
+
+    @BeforeEach
+    public void setup() {
+        profileImage = ProfileImage.builder()
+                .filePath(FILE_PATH)
+                .build();
+
+        user = User.builder()
+                .id(SOCIAL_ID)
+                .email(EMAIL)
+                .name(NAME)
+                .mannerTemperature(MANNER_TEMPERATURE)
+                .intro(INTRO)
+                .gender(GENDER)
+                .authProvider(AUTH_PROVIDER)
+                .profileImage(profileImage)
+                .build();
+
+        planImage = PlanImage.builder()
+                .filePath(FILE_PATH)
+                .build();
+
+        plan = Plan.builder()
+                .title(TITLE)
+                .content(CONTENT)
+                .category(CATEGORY)
+                .startDate(START_DATE)
+                .endDate(END_DATE)
+                .minimumAge(MINIMUM_AGE)
+                .maximumAge(MAXIMUM_AGE)
+                .address(createAddress())
+                .requireRecruitMember(getRequireRecruitMember())
+                .currentRecruitMember(CURRENT_RECRUIT_MEMBER)
+                .planImage(planImage)
+                .user(user)
+                .build();
+    }
+
+    private Address createAddress() {
+        return new Address(NATION, CITY, DETAIL, LATITUDE, LONGITUDE);
+    }
+
+    private String getRequireRecruitMember() {
+        return RECRUIT_MAN_NUMBER + "/" + RECRUIT_WOMAN_NUMBER + "/" + RECRUIT_ETC_NUMBER;
+    }
+
+}
