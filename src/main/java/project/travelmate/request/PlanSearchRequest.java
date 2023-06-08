dd2555255f2aedd66ad1a12c60ba4f6046412d1d
woong7361 <woong7361@gmail.com>
2023-06-08 21:42:06 +0900

    fix: plans get filtering element no body to param

--- src/main/java/project/travelmate/request/PlanSearchRequest.java
+++ src/main/java/project/travelmate/request/PlanSearchRequest.java
@@ -3,12 +3,14 @@ package project.travelmate.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import project.travelmate.domain.enums.Category;
 import project.travelmate.domain.enums.Gender;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class PlanSearchRequest {
     private String title;
     private Gender gender;
